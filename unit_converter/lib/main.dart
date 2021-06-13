b04cdfa81ecfd2c23e287a1dfe902fc84b352824
Duc Do <ducdxbk@gmail.com>
2021-06-13 23:32:14 +0700

    unit converter in-progress

--- unit_converter/lib/main.dart
+++ unit_converter/lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   double? _numberFrom;
+  String? _startMeasure;
+  String? _convertedMeasure;
+
   final List<String> _measures = [
     'meters',
     'kilometers',
@@ -22,9 +25,21 @@ class MyAppState extends State<MyApp> {
     'ounces',
   ];
 
+  final TextStyle inputStyle = TextStyle(
+    fontSize: 20,
+    color: Colors.blue.shade900,
+  );
+
+  final TextStyle labelStyle = TextStyle(
+    fontSize: 24,
+    color: Colors.grey.shade700,
+  );
+
   @override
   void initState() {
     _numberFrom = 0;
+    _startMeasure = "meters";
+    String? _convertedMeasure = "kilometers";
     super.initState();
   }
 
@@ -33,35 +48,101 @@ class MyAppState extends State<MyApp> {
     return MaterialApp(
       title: "Measure Converter",
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Measure Converter"),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              DropdownButton<String>(
-                  items: _measures.map((String value){
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                onChanged: (_)=>{},
-              ),
-              TextField(
-                style: TextStyle(fontSize: 28, color: Colors.black87),
-                onChanged: (text) {
-                  var rv = double.tryParse(text);
-                  setState(() {
-                    _numberFrom = rv;
-                  });
-                },
-              ),
-              Text((_numberFrom == null) ? '' : _numberFrom.toString())
-            ],
+          appBar: AppBar(
+            title: const Text("Measure Converter"),
           ),
-        ),
-      ),
+          body: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Spacer(),
+                  Text(
+                    "Value",
+                    style: labelStyle,
+                  ),
+                  Spacer(),
+                  TextField(
+                    style: inputStyle,
+                    decoration: InputDecoration(
+                        hintText: "Please input measure to convert..."),
+                    onChanged: (text) {
+                      var rv = double.tryParse(text);
+                      setState(() {
+                        _numberFrom = rv;
+                      });
+                    },
+                  ),
+                  Spacer(),
+                  Text(
+                    "From",
+                    style: labelStyle,
+                  ),
+                  Spacer(),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    style: inputStyle,
+                    items: _measures.map((String item) {
+                      return DropdownMenuItem<String>(
+                        value: item,
+                        child: Text(
+                          item,
+                          style: inputStyle,
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (item) {
+                      setState(() {
+                        _startMeasure = item;
+                      });
+                    },
+                    value: _startMeasure,
+                  ),
+                  Spacer(),
+                  Text(
+                    "To",
+                    style: labelStyle,
+                  ),
+                  Spacer(),
+                  DropdownButton<String>(
+                    isExpanded: true,
+                    items: _measures.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: inputStyle,
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _convertedMeasure = value;
+                      });
+                    },
+                    value: _convertedMeasure,
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      "Convert",
+                      style: labelStyle,
+                    ),
+                    onPressed: () => {},
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                  Text(
+                    (_numberFrom == null) ? '' : _numberFrom.toString(),
+                    style: labelStyle,
+                  ),
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
